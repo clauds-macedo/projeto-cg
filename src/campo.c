c6ea890d40f4ffba6aa32749a4878e38dde026b0
clauds-macedo <yanalogic@gmail.com>
2022-11-22 20:42:00 -0300

    me ajuda marcelo pfv

--- src/campo.c
+++ src/campo.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <math.h>
 #include <stdbool.h>
+
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 #define DIAMETRO_BOLA 0.5f
@@ -62,8 +64,41 @@ typedef struct Point {
 	GLfloat z;
 } Point;
 
+void load_texture(const char *path, int index)
+{
+	unsigned int textureID;
+    glGenTextures(1, &textureID);
 
-int width, height, numColCh;
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+	} else {
+		printf("Existe essa textura não me ajuda ai marcelo na moral");
+	}
+}
+
+void apply_texture(int index)
+{
+   glBindTexture(GL_TEXTURE_2D, index);
+}
 
 void print_vertex(GLfloat vertex[]) {
     printf("Point: (%f, %f, %f)\n", vertex[0], vertex[1], vertex[2]);
@@ -536,12 +571,16 @@ void Cube (
 void desenha_campo(GLfloat V[][3]) 
 {
     glPushMatrix();
+    	load_texture("grass.jpg", 0);
+    	glEnable(GL_TEXTURE_2D);
         // glRotatef(T, 0, 1, 0);
         glColor3f(0.133f, 0.545f, 0.133f);
         glScalef(30, 0.5, 10);
         Cube(V[0], V[1], V[2], V[3], V[4], V[5], V[6], V[7]);
+        apply_texture(0);
         fieldLines(V[0], V[1], V[5], V[4]);
         placas(V[0], V[1], V[4], V[5]);
+        glDisable(GL_TEXTURE_2D);
     glPopMatrix();
 }
 
